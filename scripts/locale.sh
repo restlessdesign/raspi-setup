7b0c235cfde039c1350be2b38b6cfb9bf798a79d
Kevin Sweeney <kevinsweeney@gmail.com>
2016-08-23 22:25:33 -0400

    Make sudo access a requirement for all scripts

--- scripts/locale.sh
+++ scripts/locale.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure program is run via sudo
+if [ "$(whoami)" != "root" ]; then
+    echo "This script must be run as the root user"
+    exit 1
+fi
+
 echo "Updating keyboard layout…"
 sed -i "s/XKBLAYOUT=\"gb\"/XKBLAYOUT=\"us\"/" /etc/default/keyboard
 
--- scripts/network.sh
+++ scripts/network.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure program is run via sudo
+if [ "$(whoami)" != "root" ]; then
+    echo "This script must be run as the root user"
+    exit 1
+fi
+
 # Setup network
 read -p "Setup wireless network? [Y/n]\n" setup_wireless
 setup_wireless=${setup_wireless,,} # convert response to lowercase
