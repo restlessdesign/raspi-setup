3e76c53a37d95446f6b6609e6ff4f13a41710c4e
Kevin Sweeney <restlessdesign@users.noreply.github.com>
2016-08-23 22:23:46 -0400

    Separate concepts of updating vs. installing

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ensure program is run via sudo
+if [ "$(whoami)" != "root" ]; then
+    echo "This script must be run as the root user"
+    exit 1
+fi
+
+# Install common packages
+apt-get -y install fail2ban
+apt-get -y install htop
+apt-get -y install logwatch
+# @todo
+# echo "/usr/sbin/logwatch --output mail --mailto kevinsweeney@gmail.com --detail high" >> /etc/cron.daily/00logwatch
+apt-get -y install vim
+apt-get -y install xclip
