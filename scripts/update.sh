5c6d5ab7adcb4b6832b0c5abebb0bc64ddebbc78
Kevin Sweeney <kevinsweeney@gmail.com>
2016-08-23 22:23:46 -0400

    Separate concepts of updating vs. installing

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,12 +12,3 @@ rpi-update
 # Update packages
 apt-get update
 apt-get -y dist-upgrade
-
-# Install common packages
-apt-get -y install fail2ban
-apt-get -y install htop
-apt-get -y install logwatch
-# @todo
-# echo "/usr/sbin/logwatch --output mail --mailto kevinsweeney@gmail.com --detail high" >> /etc/cron.daily/00logwatch
-apt-get -y install vim
-apt-get -y install xclip
