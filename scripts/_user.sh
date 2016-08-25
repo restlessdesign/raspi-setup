93f476c7bfbdca330690324031e4339b487c6ff3
Kevin Sweeney <restlessdesign@users.noreply.github.com>
2016-08-25 11:30:59 -0400

    Clean up user creation process
    - Fix echo formatting
    - Copy .bash_aliases from pi user’s directory
    - Remove xclip install dependency since it requires x

--- scripts/_user.sh
+++ scripts/_user.sh
@@ -6,29 +6,39 @@ if [ "$(whoami)" != "root" ]; then
     exit 1
 fi
 
-read -p "Create new user? [Y/n]\n" create_user
+read -p "Create new user? [Y/n] " create_user
 create_user=${create_user,,}
 if [[ $create_user =~ ^(yes|y)$ ]]; then
     # Create new user
-    echo "Enter desired login details below:\n"
-
     read -p "Username: " user
-    read -p "Password: " pass
 
     useradd -m -G $(groups | cut -f 1 -d ' ' --complement | sed "s/ /,/g") ${user}
     passwd ${user}
-    echo "User ${user} successfully created!\n"
+    echo "User ${user} successfully created!"
     echo ""
-    echo "You must manually add the user “${user}” to the sudoers file."
-    echo "${user}    ALL=(ALL) ALL" | xcopy
-    echo "The necessary text has been copied to your clipboard."
-    echo "`visudo` will now execute. Paste the text below line which looks like:"
-    echo "  root    ALL=(ALL) ALL"
+    echo "You must manually add the user “${user}” to the sudoers file. Copy the following line:"
+    echo ""
+    echo "${user}    ALL=(ALL) ALL"
+    echo ""
+    read -p "Once you have copied the line above, press [Enter] to continue…"
+
+    echo "visudo will now execute. Paste the text below line which looks like:"
+    echo ""
+    echo "root    ALL=(ALL) ALL"
+    echo ""
     echo "When you are done, exit the editor to continue running this script."
     echo ""
+    read -p "Press [Enter] to continue…"
+    visudo
+    echo ""
+
+    # Copy dotfiles
+    echo "Copying .bash_aliases file…"
+    cp /home/pi/.bash_aliases ~/.bash_aliases
+    source ~/.bash_aliases
 
     # Delete default user
-    read -p "Delete default user, “pi”? (this is strongly recommended!) [Y/n]\n" remove_default_user
+    read -p "Delete default user, “pi”? (this is strongly recommended!) [Y/n] " remove_default_user
     remove_default_user=${remove_default_user,,}
     if [[ $remove_default_user =~ ^(yes|y)$ ]]; then
         deluser --remove-all-files pi
